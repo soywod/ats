9852c6fb85d512b3cfd1c84d38e21375f9c83136
Clément DOUIN <soywod@users.noreply.github.com>
2020-11-24 22:07:10 +0100

    make nav sticky

--- src/_shared/nav/index.ts
+++ src/_shared/nav/index.ts
@@ -3,9 +3,29 @@ import style from "./index.css";
 import tpl from "./index.html";
 
 export class ATSNav extends HTMLElement {
-  constructor() {
+  private sticky = false;
+
+  public constructor() {
     super();
     this.attachShadow({mode: "open"}).append(parseStyle(style), parseTpl(tpl));
+  }
+
+  private handleScroll = () => {
+    if (window.scrollY === 0) {
+      this.removeAttribute("sticky");
+      this.sticky = false;
+    } else if (!this.sticky) {
+      this.setAttribute("sticky", "");
+      this.sticky = true;
+    }
+  };
+
+  protected connectedCallback() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+
+  protected disconnectedCallback() {
+    window.removeEventListener("scroll", this.handleScroll);
   }
 }
 
