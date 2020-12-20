f39d58c0b06b22f8fc7546606b4eb5ad70a976d9
Clément DOUIN <soywod@users.noreply.github.com>
2020-12-21 00:50:41 +0100

    animate home section 6 imgs

---
+++ src/_shared/img/index.ts
@@ -0,0 +1,34 @@
+import {parseStyle} from "../dom-utils";
+import style from "./index.css";
+
+export class ATSImg extends HTMLImageElement {
+  private intersection$: IntersectionObserver;
+  private threshold: number;
+
+  public constructor() {
+    super();
+    this.threshold = parseFloat(this.getAttribute("threshold") || "0");
+
+    this.intersection$ = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            this.setAttribute("intersecting", "");
+          }
+        });
+      },
+      {threshold: this.threshold},
+    );
+  }
+
+  protected connectedCallback() {
+    this.intersection$.observe(this);
+  }
+
+  protected disconnectedCallback() {
+    this.intersection$.unobserve(this);
+  }
+}
+
+customElements.define("ats-img", ATSImg, {extends: "img"});
+customElements.whenDefined("ats-img").then(() => document.head.append(parseStyle(style)));
--- src/home/index.ts
+++ src/home/index.ts
@@ -2,6 +2,7 @@ import "../_shared/container";
 import "../_shared/nav";
 import "../_shared/carousel";
 import "../_shared/autoscroller";
+import "../_shared/img";
 import "../_shared/frame";
 import "../_shared/footer";
 
