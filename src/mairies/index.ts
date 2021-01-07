7c33d71d42b5888d397a44b17fa54d726c4fd55c
Clément DOUIN <soywod@users.noreply.github.com>
2021-01-07 14:13:33 +0100

    improve mairies icons + btn section 3

--- src/mairies/index.ts
+++ src/mairies/index.ts
@@ -9,18 +9,11 @@ import "../_shared/footer";
 import section2cImg from "./section-2-c.jpeg";
 import section2dImg from "./section-2-d.jpeg";
 
-import {cssOfStr} from "../_shared/dom-utils";
+import {findOrFail, cssOfStr} from "../_shared/dom-utils";
 import css from "./index.css";
 import cssLg from "./index.lg.css";
 
-const section2c = document.getElementById("section-2-c");
-if (section2c) {
-  section2c.style.backgroundImage = `url(${section2cImg})`;
-}
-
-const section2d = document.getElementById("section-2-d");
-if (section2d) {
-  section2d.style.backgroundImage = `url(${section2dImg})`;
-}
+findOrFail(document, HTMLDivElement, "section-2-c").style.backgroundImage = `url(${section2cImg})`;
+findOrFail(document, HTMLDivElement, "section-2-d").style.backgroundImage = `url(${section2dImg})`;
 
 document.head.append(cssOfStr(css), cssOfStr(cssLg));
