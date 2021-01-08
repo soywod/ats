ede01f651f942d872594e96c57f42772b90b76c4
Clément DOUIN <soywod@users.noreply.github.com>
2021-01-08 12:53:22 +0100

    integrate mairies s6, improve intersection box

--- src/_shared/intersection-box/index.ts
+++ src/_shared/intersection-box/index.ts
@@ -5,43 +5,30 @@ import css from "./index.css";
 import tpl from "./index.html";
 
 export class ATSIntersectionBox extends HTMLElement {
-  /**
-   * Intersection threshold (between 0 and 1)
-   */
   private threshold: number;
-
-  /**
-   * Intersection observable
-   */
-  private obs$: IntersectionObserver;
 
   public constructor() {
     super();
     this.attachShadow({mode: "open"}).append(cssOfStr(css), tplOfStr(tpl));
     this.threshold = parseFloat(this.getAttribute("threshold") || "") || 0;
-
-    this.obs$ = new IntersectionObserver(
-      entries => {
-        entries.forEach(entry => {
-          console.log(entry);
-
-          if (entry.isIntersecting) {
-            this.setAttribute("intersecting", "");
-          } else {
-            this.removeAttribute("intersecting");
-          }
-        });
-      },
-      {threshold: [this.threshold]},
-    );
   }
 
+  private handleScroll = () => {
+    const threshold = 1 - this.getBoundingClientRect().top / window.innerHeight;
+
+    if (threshold > this.threshold) {
+      this.setAttribute("visible", "");
+    } else {
+      this.removeAttribute("visible");
+    }
+  };
+
   protected connectedCallback() {
-    this.obs$.observe(this);
+    window.addEventListener("scroll", this.handleScroll);
   }
 
   protected disconnectedCallback() {
-    this.obs$.unobserve(this);
+    window.removeEventListener("scroll", this.handleScroll);
   }
 }
 
