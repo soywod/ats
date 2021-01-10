dfd901f9d3e81b47093fa1f2fdcdde3c244be41d
Clément DOUIN <soywod@users.noreply.github.com>
2021-01-10 17:56:16 +0100

    improve parallaxe, add mairie intersection for all sections

--- src/_shared/parallaxe-box/index.ts
+++ src/_shared/parallaxe-box/index.ts
@@ -2,6 +2,19 @@ import {cssOfStr, tplOfStr} from "../dom-utils";
 import css from "./index.css";
 import tpl from "./index.html";
 
+type Type = "translate-x" | "rotate-y";
+
+function isType(type: any): type is Type {
+  switch (type) {
+    case "translate-x":
+    case "rotate-y":
+      return true;
+
+    default:
+      return false;
+  }
+}
+
 type Direction = "normal" | "reverse";
 
 function isDirection(direction: any): direction is Direction {
@@ -9,15 +22,19 @@ function isDirection(direction: any): direction is Direction {
 }
 
 export class ATSParallaxeBox extends HTMLElement {
-  private value = 1;
+  private ratio = 1;
+  private type: Type = "translate-x";
   private direction: Direction = "normal";
 
   public constructor() {
     super();
     this.attachShadow({mode: "open"}).append(cssOfStr(css), tplOfStr(tpl));
 
-    const value = this.getAttribute("value");
-    if (value) this.value = parseFloat(value);
+    const ratio = this.getAttribute("ratio");
+    if (ratio) this.ratio = parseFloat(ratio);
+
+    const type = this.getAttribute("type");
+    if (isType(type)) this.type = type;
 
     const direction = this.getAttribute("direction");
     if (isDirection(direction)) this.direction = direction;
@@ -25,8 +42,21 @@ export class ATSParallaxeBox extends HTMLElement {
 
   private handleMouseOver = (evt: MouseEvent) => {
     const direction = this.direction === "normal" ? 1 : -1;
-    const deltaX = (direction * (this.clientWidth * 0.5 - evt.clientX)) / this.value;
-    this.style.transform = `scale(1.05) translateX(${deltaX}px)`;
+    const deltaX = direction * (this.clientWidth * 0.5 - evt.clientX) * this.ratio;
+    this.style.transform = (() => {
+      const scale = "scale(1.05)";
+
+      switch (this.type) {
+        case "translate-x":
+          return `${scale} translateX(${deltaX}px)`;
+
+        case "rotate-y":
+          return `${scale} perspective(120rem) rotateY(${deltaX}deg)`;
+
+        default:
+          return scale;
+      }
+    })();
   };
 
   protected connectedCallback() {
