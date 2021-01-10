33ce81dc17a40cb9c559a1409f258961c82c7df0
Clément DOUIN <soywod@users.noreply.github.com>
2021-01-10 23:55:02 +0100

    integrate travaux publics page

--- .webpack.prod.js
+++ .webpack.prod.js
@@ -1,71 +1,73 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = ["home", "mairies", "caves-et-cooperatives", "logistique-et-industrie"].map(page => {
-  return {
-    mode: "production",
-    entry: path.resolve(__dirname, "src", page, "index.ts"),
-    output: {
-      path: path.resolve(__dirname, "dist", page),
-      filename: "index.js",
-    },
-    resolve: {
-      extensions: [".js", ".ts"],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.html$/i,
-          use: {
-            loader: "html-loader",
-            options: {
-              attributes: {
-                list: [
-                  {
-                    tag: "link",
-                    attribute: "href",
-                    type: "src",
-                  },
-                  {
-                    tag: "img",
-                    attribute: "src",
-                    type: "src",
-                  },
-                  {
-                    tag: "source",
-                    attribute: "src",
-                    type: "src",
-                  },
-                ],
+module.exports = ["home", "mairies", "caves-et-cooperatives", "logistique-et-industrie", "travaux-publics"].map(
+  page => {
+    return {
+      mode: "production",
+      entry: path.resolve(__dirname, "src", page, "index.ts"),
+      output: {
+        path: path.resolve(__dirname, "dist", page),
+        filename: "index.js",
+      },
+      resolve: {
+        extensions: [".js", ".ts"],
+      },
+      module: {
+        rules: [
+          {
+            test: /\.html$/i,
+            use: {
+              loader: "html-loader",
+              options: {
+                attributes: {
+                  list: [
+                    {
+                      tag: "link",
+                      attribute: "href",
+                      type: "src",
+                    },
+                    {
+                      tag: "img",
+                      attribute: "src",
+                      type: "src",
+                    },
+                    {
+                      tag: "source",
+                      attribute: "src",
+                      type: "src",
+                    },
+                  ],
+                },
               },
             },
           },
-        },
-        {
-          test: /global\.css$/i,
-          use: ["file-loader", "extract-loader"],
-        },
-        {
-          test: /\.css$/i,
-          use: ["css-loader"],
-        },
-        {
-          test: /\.(jpeg|png|svg|mp4)$/i,
-          use: "file-loader",
-        },
-        {
-          test: /\.(j|t)s$/i,
-          exclude: /node_modules/,
-          use: "babel-loader",
-        },
+          {
+            test: /global\.css$/i,
+            use: ["file-loader", "extract-loader"],
+          },
+          {
+            test: /\.css$/i,
+            use: ["css-loader"],
+          },
+          {
+            test: /\.(jpeg|png|svg|mp4)$/i,
+            use: "file-loader",
+          },
+          {
+            test: /\.(j|t)s$/i,
+            exclude: /node_modules/,
+            use: "babel-loader",
+          },
+        ],
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: path.resolve(__dirname, "src", page, "index.html"),
+          filename: "index.html",
+          inject: "body",
+        }),
       ],
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "src", page, "index.html"),
-        filename: "index.html",
-        inject: "body",
-      }),
-    ],
-  };
-});
+    };
+  },
+);
---
+++ src/travaux-publics/index.ts
@@ -0,0 +1,15 @@
+import "../_shared/advise-section";
+import "../_shared/anchor-header";
+import "../_shared/autoscroller";
+import "../_shared/carousel";
+import "../_shared/container";
+import "../_shared/footer";
+import "../_shared/frame";
+import "../_shared/img";
+import "../_shared/intersection-box";
+import "../_shared/lazy-section";
+import "../_shared/nav";
+import "../_shared/parallaxe-box";
+
+import smoothscroll from "smoothscroll-polyfill";
+smoothscroll.polyfill();
