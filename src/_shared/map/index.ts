d45c7fbdd2149c4ca7ba09eb0b462201e16c1d01
Clément DOUIN <soywod@users.noreply.github.com>
2021-01-21 00:02:46 +0100

    replace travaux publics anim

--- src/_shared/map/index.ts
+++ src/_shared/map/index.ts
@@ -4,6 +4,13 @@ import {cssOfStr, tplOfStr, findOrFail} from "../dom-utils";
 import css from "./index.css";
 import tpl from "./index.html";
 
+import iconUrl from "./marker.svg";
+
+console.log(iconUrl);
+
+const lat = 43.4481999;
+const lng = 3.4144988;
+
 class ATSMap extends HTMLElement {
   private map: Leaflet.Map;
 
@@ -12,11 +19,12 @@ class ATSMap extends HTMLElement {
     const root = this.attachShadow({mode: "open"});
     root.append(cssOfStr(css), tplOfStr(tpl));
 
-    this.map = Leaflet.map(findOrFail(root, HTMLDivElement, "map")).setView([43.4481999, 3.4144988], 15);
+    this.map = Leaflet.map(findOrFail(root, HTMLDivElement, "map")).setView([lat, lng], 15);
     this.map.scrollWheelZoom.disable();
 
     // TODO: add icon + popup
-    /* new Leaflet.Marker({lat: 51.505, lng: -0.09}).addTo(this.map); */
+    new Leaflet.Marker({lat, lng}, {icon: new Leaflet.Icon({iconUrl, iconSize: [50, 50]})}).addTo(this.map);
+
     Leaflet.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: "&copy; <a href=http://osm.org/copyright>OpenStreetMap</a> contributors",
     }).addTo(this.map);
